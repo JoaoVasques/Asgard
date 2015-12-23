c462ae1705b8e1475ab9e5dbeff1ceb02440ddc9
Joao Vasques <joao.vasques@tecnico.ulisboa.pt>
2015-12-23 22:45:25 +0000

    Start Emacs on fullcreen by default
    Starts Emacs always on fullcreen

--- settings/my-core.el
+++ settings/my-core.el
@@ -87,3 +87,12 @@
     (require 'helm)
   (package-install 'helm)
     )
+
+(defun toggle-fullscreen ()
+  "Toggle full screen"
+  (interactive)
+  (set-frame-parameter
+     nil 'fullscreen
+     (when (not (frame-parameter nil 'fullscreen)) 'fullboth)))
+
+(set-frame-parameter nil 'fullscreen 'fullboth)
