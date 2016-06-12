05618fcf83382bbdc6309b9f9fd36907b4b23165
Joao Vasques <joao.vasques@tecnico.ulisboa.pt>
2016-06-12 21:53:26 +0100

    Modularize asgard config
    Removes some uncessary modules and modularises components.

---
+++ settings/asg-common.el
@@ -0,0 +1,19 @@
+(provide 'asg-common)
+
+(defun asg-load-packages (packages-list)
+  "Load all packages in packages-list"
+  (dolist (package packages-list)
+    (unless (package-installed-p package)
+      (package-install package))
+    (require 'package)))
+
+(defun asg-require-list-modules (modules)
+  (dolist (module modules)
+    (require 'module)))
+
+(defun is-mac-osx? ()
+  "Checks if the current OS is Mac OS"
+  (eq system-type 'darwin))
+
+(defun asg-command-exists? (cmd)
+  (eq (shell-command cmd) 0))
--- settings/languages/asg-elixir.el
+++ settings/languages/asg-elixir.el
@@ -2,20 +2,22 @@
 
 (provide 'asg-elixir)
 
-(setq-local my-elixir-packages '(elixir-mode alchemist))
+(require 'asg-common)
 
-(dolist (package my-elixir-packages)
-  (unless (package-installed-p package)
-    (package-install package)))
+(defun load-asg-elixir ()
+  (cond ((asg-command-exists? "which iex")
+         (asg-load-packages '(elixir-mode alchemist))
+         (setq-local my-elixir-packages '(elixir-mode alchemist))
 
-(require 'alchemist)
-(require 'elixir-mode)
+         ;; Mix setup
+         (setq alchemist-mix-command "/usr/local/bin/mix")
+         (setq alchemist-iex-program-name "/usr/local/bin/iex")
+         (setq alchemist-execute-command "/usr/local/bin/elixir")
+         (setq alchemist-compile-command "/usr/local/bin/elixirc")
 
-;; Mix setup
-(setq alchemist-mix-command "/usr/local/bin/mix")
-(setq alchemist-iex-program-name "/usr/local/bin/iex")
-(setq alchemist-execute-command "/usr/local/bin/elixir")
-(setq alchemist-compile-command "/usr/local/bin/elixirc")
+         ;; Test mode
+         (setq alchemist-test-display-compilation-output t))
+	))
 
-;; Test mode
-(setq alchemist-test-display-compilation-output t)
+(load-asg-elixir)
+
